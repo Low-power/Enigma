dea0745fa083dc7d31195b5d897dea2f4bd8caff
WHR <msl0000023508@gmail.com>
2015-02-10 19:06:51 +0800

    Clean up; Add some copyright and license notes;

--- server/main.cpp
+++ server/main.cpp
@@ -1,3 +1,12 @@
+/*	A part of Enigma
+	Copyright 2014 Ixil
+	Copyright 2015 libdll.so
+
+	This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+*/
+
 #include "mainwindow.h"
 #include "model.h"
 #include <QtGui/QApplication>
@@ -20,10 +29,7 @@ int main(int argc, char *argv[])
 	QTextCodec::setCodecForTr(codec);
 
 	QApplication a(argc, argv);
-
 	MainWindow w;
-
 	w.show();
-
 	return a.exec();
 }
